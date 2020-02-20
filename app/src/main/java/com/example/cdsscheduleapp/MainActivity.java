9bc0c9a867b2295f03f6ca059625994e2257a125
CodingLaner <codingLaner@users.noreply.github.com>
2020-02-20 17:48:16 +0900

    Fragment4Memo.java 프래그먼트 4번째 메모 생성 및 fragment4_memo.xml과 연결
    MainActivity.java
    하단 네비게이션 바에 대한 버튼 클릭시 리스너와 프래그먼트 연결
    
    fragment4_memo.xml
    프래그먼트 4번째 xml 생성
    
    navigation_items.xml
    id 오타 수정

--- app/src/main/java/com/example/cdsscheduleapp/MainActivity.java
+++ app/src/main/java/com/example/cdsscheduleapp/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.cdsscheduleapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
 import com.example.cdsscheduleapp.fragment1todo.Fragment1ToDo;
+import com.example.cdsscheduleapp.fragment4memo.Fragment4Memo;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentCallback, BottomNavigationView.OnNavigationItemSelectedListener {
     Fragment1ToDo fragment1;
+    Fragment4Memo fragment4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +23,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         fragment1 = new Fragment1ToDo();
+        fragment4 = new Fragment4Memo();
 
         getSupportFragmentManager().beginTransaction().add(R.id.container, fragment1).commit();
         // 추후 설정에서 앱 시작시 프래그먼트 변경하는 기능 추가 고려
 
-
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.tab1:
+                onFragementSelected(0, null);
+                return true;
+            case R.id.tab2:
+                onFragementSelected(1, null);
+                return true;
+            case R.id.tab3:
+                onFragementSelected(2, null);
+                return true;
+            case R.id.tab4:
+                onFragementSelected(3, null);
+                return true;
+            case R.id.tab5:
+                onFragementSelected(4, null);
+                return true;
+        }
 
+        return false;
+    }
+
+    @Override
+    public void onFragementSelected(int position, Bundle bundle) {
+        Fragment curFragment = null;
+
+        switch (position) {
+            case 0: curFragment = fragment1;
+            case 3: curFragment = fragment4;
+        }
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, curFragment).commit();
+    }
 }
---
+++ app/src/main/java/com/example/cdsscheduleapp/fragment4memo/Fragment4Memo.java
@@ -0,0 +1,23 @@
+package com.example.cdsscheduleapp.fragment4memo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.cdsscheduleapp.R;
+
+public class Fragment4Memo extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment4_memo, container, false);
+
+        return viewGroup;
+    }
+}
